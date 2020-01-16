7d97e2073127110cc25fc4084eb3307c78761425
JackBai2000 <41300964+JackBai2000@users.noreply.github.com>
2020-01-15 19:14:45 -0500

    Organizational Changes
    Split the index.html into the respective files peng.html, peng.css, peng.js to keep HTML CSS and JS separate from each other. This will make edits and further revisions easier as well as keeping things organized.

---
+++ peng.js
@@ -0,0 +1,209 @@
+//Buttons for Summoner 1
+
+function zephyr1() {
+    var button = document.getElementById("zephyr1");
+    if (button.value == "Yes") {
+        button.value = "No";
+        button.style.background = '#ff0000';
+    }
+    else {
+        button.value = "Yes";
+        button.style.background = '#00ff00';
+    }
+
+}
+
+function hide1() {
+    var summoner1Info = document.getElementById("hide1");
+    var hide1Button = document.getElementById("hideButton1");
+    if (summoner1Info.style.display === "none") {
+        summoner1Info.style.display = "block";
+        hide1Button.value = "Hide";
+    }
+    else {
+        summoner1Info.style.display = "none";
+        hide1Button.value = "Unhide";
+    }
+}
+
+
+
+
+//Buttons for Summoner 2
+
+function zephyr2() {
+    var button = document.getElementById("zephyr2");
+    if (button.value == "Yes") {
+        button.value = "No";
+        button.style.background = '#ff0000';
+    }
+    else {
+        button.value = "Yes";
+        button.style.background = '#00ff00';
+    }
+
+}
+
+
+function hide2() {
+    var summoner2Info = document.getElementById("hide2");
+    var hide2Button = document.getElementById("hideButton2");
+    if (summoner2Info.style.display === "none") {
+        summoner2Info.style.display = "block";
+        hide2Button.value = "Hide";
+    }
+    else {
+        summoner2Info.style.display = "none";
+        hide2Button.value = "Unhide";
+    }
+}
+
+//Buttons for Summoner 3
+
+function zephyr3() {
+    var button = document.getElementById("zephyr3");
+    if (button.value == "Yes") {
+        button.value = "No";
+        button.style.background = '#ff0000';
+    }
+    else {
+        button.value = "Yes";
+        button.style.background = '#00ff00';
+    }
+
+}
+
+
+function hide3() {
+    var summoner3Info = document.getElementById("hide3");
+    var hide3Button = document.getElementById("hideButton3");
+    if (summoner3Info.style.display === "none") {
+        summoner3Info.style.display = "block";
+        hide3Button.value = "Hide";
+    }
+    else {
+        summoner3Info.style.display = "none";
+        hide3Button.value = "Unhide";
+    }
+}
+
+
+//Buttons for Summoner 4
+
+function zephyr4() {
+    var button = document.getElementById("zephyr4");
+    if (button.value == "Yes") {
+        button.value = "No";
+        button.style.background = '#ff0000';
+    }
+    else {
+        button.value = "Yes";
+        button.style.background = '#00ff00';
+    }
+
+}
+
+
+function hide4() {
+    var summoner4Info = document.getElementById("hide4");
+    var hide4Button = document.getElementById("hideButton4");
+    if (summoner4Info.style.display === "none") {
+        summoner4Info.style.display = "block";
+        hide4Button.value = "Hide";
+    }
+    else {
+        summoner4Info.style.display = "none";
+        hide4Button.value = "Unhide";
+    }
+}
+
+
+
+//Buttons for Summoner 5
+function zephyr5() {
+    var button = document.getElementById("zephyr5");
+    if (button.value == "Yes") {
+        button.value = "No";
+        button.style.background = '#ff0000';
+    }
+    else {
+        button.value = "Yes";
+        button.style.background = '#00ff00';
+    }
+
+}
+
+
+
+function hide5() {
+    var summoner5Info = document.getElementById("hide5");
+    var hide5Button = document.getElementById("hideButton5");
+    if (summoner5Info.style.display === "none") {
+        summoner5Info.style.display = "block";
+        hide5Button.value = "Hide";
+    }
+    else {
+        summoner5Info.style.display = "none";
+        hide5Button.value = "Unhide";
+    }
+}
+
+
+//Buttons for Summoner 6
+
+
+function zephyr6() {
+    var button = document.getElementById("zephyr6");
+    if (button.value == "Yes") {
+        button.value = "No";
+        button.style.background = '#ff0000';
+    }
+    else {
+        button.value = "Yes";
+        button.style.background = '#00ff00';
+    }
+
+}
+
+
+function hide6() {
+    var summoner6Info = document.getElementById("hide6");
+    var hide6Button = document.getElementById("hideButton6");
+    if (summoner6Info.style.display === "none") {
+        summoner6Info.style.display = "block";
+        hide6Button.value = "Hide";
+    }
+    else {
+        summoner6Info.style.display = "none";
+        hide6Button.value = "Unhide";
+    }
+}
+
+//Buttons for Summoner 7
+function zephyr7() {
+    var button = document.getElementById("zephyr7");
+    if (button.value == "Yes") {
+        button.value = "No";
+        button.style.background = '#ff0000';
+    }
+    else {
+        button.value = "Yes";
+        button.style.background = '#00ff00';
+    }
+
+}
+
+
+function hide7() {
+    var summoner7Info = document.getElementById("hide7");
+    var hide7Button = document.getElementById("hideButton7");
+    if (summoner7Info.style.display === "none") {
+        summoner7Info.style.display = "block";
+        hide7Button.value = "Hide";
+    }
+    else {
+        summoner7Info.style.display = "none";
+        hide7Button.value = "Unhide";
+    }
+}
+
